f4aa7becd1634ae7d630761a2c54dfda34dd79c8
Angelova <Margarita.Angelova@adesso.bg>
2021-07-12 12:04:36 +0300

    Deploying the application backend

--- db/mongoose.js
+++ db/mongoose.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //port 27017 is the default port for mongodb + '/name of our database(TaskManager):
-mongoose.connect('mongodb://localhost:27017/MedTracker', { useNewUrlParser: true }).then(() => {
-// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }).then(() => {
+// mongoose.connect('mongodb://localhost:27017/MedTracker', { useNewUrlParser: true }).then(() => {
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }).then(() => {
     console.log("Connected to MongoDB successfully :)");
 
     console.log(process.env.DB_URL);
